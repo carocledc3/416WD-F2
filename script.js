af5af65e4f7b71aa6deda0b983b39fbf82975c21
carocledc3 <carocledc3>
2026-04-16 05:35:52 +0800

    Added link to source code; added code signature in the relevant files

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// CARLOS ROMEO CLEMENTE DEL CASTILLO III
+// UNIVERSITY OF THE CORDILLERAS, BSIT-2
+// 416WD/CC14/WEB APPLICATION DEVELOPMENT
+// 416WD-F2 Product Landing Page Project
+// 16 APRIL 2026 THURSDAY
+
+
 $("#book").submit(function(e) {
     e.preventDefault();
     });
